6c2264c3b7a213283a9245020b03e186c61b6f41
baohanle <135090141+baohanle@users.noreply.github.com>
2023-07-20 00:45:08 +0000

    completed fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
 # add your code here
 
+numbers = list(range(1,101))
+
+for x in numbers:
+    if x % 15 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
